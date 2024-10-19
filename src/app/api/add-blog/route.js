5723642fbad6d000e8405f7efe37552b816e905e
napharsh <harshgoel18122001@gmail.com>
2024-10-19 14:00:42 +0530

    Add blog api route completed

---
+++ src/app/api/add-blog/route.js
@@ -0,0 +1,48 @@
+import connectDB from "@/database";
+import Blog from "@/models/blog";
+import Joi from "joi";
+import { NextResponse } from "next/server";
+
+
+const AddNewBlog = Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required()
+})
+
+export async function POST(req){
+    try {
+        await connectDB();
+        const extractBlogData = await req.json();
+        const {title, description} = extractBlogData;
+        const {error}  = AddNewBlog.validate({
+            title, description
+        })
+        if(error){
+            return NextResponse.json({
+                success:false,
+                message: error.details[0].message
+            })
+        }
+        const newlyCreatedBlog = await Blog.create(extractBlogData);
+        if(newlyCreatedBlog){
+            return NextResponse.json({
+                success:true,
+                message:'Blog added successfully'
+            })
+        }else{
+            return NextResponse.json({
+                success: false,
+                message: 'Something went wrong, please try again'
+            }) 
+        }
+
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({
+            success: false,
+            message: 'Something went wrong, please try again'
+        })
+        // return NextResponse
+    }
+}
